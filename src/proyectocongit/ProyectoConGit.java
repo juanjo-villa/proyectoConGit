3c230f6908a80bad699950be57258f054b1bc28a
juanjo-villa <134659772+juanjo-villa@users.noreply.github.com>
2024-07-23 18:33:51 -0500

    cambios significativos

--- src/proyectocongit/ProyectoConGit.java
+++ src/proyectocongit/ProyectoConGit.java
@@ -15,6 +15,8 @@ public class ProyectoConGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello world");
     }
     
 }
